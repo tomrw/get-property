350e81785a2bdc1475365ae57d8de6e1ce0bf545
tomrw <tomrichardwest@gmail.com>
2017-08-06 10:37:09 +0100

    Retrieve an objects property value using a stringified path

--- src/get-property.js
+++ src/get-property.js
@@ -1,1 +1,9 @@
-module.exports = 10;
+module.exports = (obj, propertyPath) => {
+	const properties = propertyPath.split('.');
+
+	return properties.reduce((obj, prop) => {
+		if (obj[prop]) {
+			return obj[prop];
+		}
+	}, obj);
+};
--- test/get-property.spec.js
+++ test/get-property.spec.js
@@ -1,8 +1,61 @@
 const expect = require('chai').expect;
 const getProperty = require('../src/get-property');
 
-describe('test', () => {
-	it('should equal 10', () => {
-		expect(getProperty).to.equal(10);
+describe('get-property', () => {
+	it('should return the expected top-level value', () => {
+		const obj = {
+			a: 1,
+			b: 2,
+			c: 3
+		};
+		const result = getProperty(obj, 'a');
+
+		expect(result).to.equal(1);
+	});
+
+	it('should return a nested value', () => {
+		const obj = {
+			a: {
+				b: 1,
+				c: 2
+			}
+		};
+		const result = getProperty(obj, 'a.b');
+
+		expect(result).to.equal(1);
+	});
+
+	it('should return a deeply nested value', () => {
+		const obj = {
+			a: {
+				b: {
+					c: {
+						d: {
+							e: 3,
+							f: 'g'
+						}
+					},
+					h: 'i'
+				}
+			}
+		};
+		const result = getProperty(obj, 'a.b.c.d');
+
+		expect(result).to.deep.equal({
+			e: 3,
+			f: 'g'
+		});
+	});
+
+	it('should return undefined when a property in the object does not exist', () => {
+		const obj = {
+			a: {
+				b: 'c'
+			}
+		};
+
+		const result = getProperty(obj, 'a.d');
+
+		expect(result).to.be.undefined;
 	});
 });
